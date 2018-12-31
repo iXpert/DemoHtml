40b0e2dbee758ca5522b5187b79ff9859f374639
ixpert <amr.elnagar@outlook.com>
2018-12-31 13:00:22 +0100

    Initial commit

---
+++ src/main/java/com/ixpert/demohtml/DemohtmlApplication.java
@@ -0,0 +1,14 @@
+package com.ixpert.demohtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemohtmlApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/ixpert/demohtml/DemohtmlApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ixpert.demohtml;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemohtmlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
